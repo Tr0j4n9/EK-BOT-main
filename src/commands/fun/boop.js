e6c85fb56ed38743cfa1b125bb59ebcc1ac390e1
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-20 17:30:22 +1300

    Update boop.js

--- src/commands/fun/boop.js
+++ src/commands/fun/boop.js
@@ -1,18 +1,16 @@
 import Promise from 'bluebird';
 
-function coin() {
+function boop() {
   const number = Math.floor(Math.random() * 2) + 1;
   if (number === 1) return Promise.resolve({upload: './images/Heads.png'});
   return Promise.resolve({upload: './images/Tails.png'});
 }
 
 export default {
-  coin,
-  coinflip: coin,
-  flip: coin,
-  flipcoin: coin
+  boop,
+  boops: coin,
 };
 
 export const help = {
-  coin: {}
+  boop: {}
 };
